ac6cbc1187d2a052fa30a7887e49740cd7c387ed
Carmine DiMascio <dimascio@us.ibm.com>
2017-01-02 14:13:10 -0500

    vagrant file with ubuntu trusty

---
+++ provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+apt-get -y update
+
+# apt-get using dpkg (debian package) behind the scenes
+# here dpk -s checks if npm is installed
+
+# if not installed (dpg will return a non-0 exit code)
+# when non-zero code is encounterd, it will hit the || block and install it
+# use /dev/null to remove dkg output
+dpkg -s nodejs &>/dev/null || {
+	apt-get install -y curl
+
+	curl -sL https://deb.nodesource.com/setup_7.x | bash -
+	apt-get install -y nodejs
+
+	# create alias from nodejs to node
+	ln -s /usr/bin/nodejs /usr/bin/node
+}
+
+command -v yo &>/dev/null || {
+	npm install -g yo generator-hubot
+}
